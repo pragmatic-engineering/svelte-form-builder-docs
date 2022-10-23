c4062ef2806bad72e94143c7ec8eb38987ba2d04
Jojoshua <jojoshua@gmail.com>
2022-10-23 13:18:14 -0400

    update final site

--- astro.config.mjs
+++ astro.config.mjs
@@ -11,10 +11,13 @@ import mdx from "@astrojs/mdx";
 // https://astro.build/config
 export default defineConfig({
   integrations: [
-  // Enable Preact to support Preact JSX components.
-  preact(),
-  // Enable React for the Algolia search component.
-  react(), svelte(), mdx()],
+    // Enable Preact to support Preact JSX components.
+    preact(),
+    // Enable React for the Algolia search component.
+    react(),
+    svelte(),
+    mdx(),
+  ],
   markdown: {
     shikiConfig: {
       // Choose from Shiki's built-in themes (or add your own)
@@ -25,8 +28,8 @@ export default defineConfig({
       // https://github.com/shikijs/shiki/blob/main/docs/languages.md
       langs: ["js", "ts", "svelte"],
       // Enable word wrap to prevent horizontal scrolling
-      wrap: true
-    }
+      wrap: true,
+    },
   },
-  site: `http://astro.build`
+  site: `https://svelte-form-builder-docs.vercel.app/en/home`,
 });
